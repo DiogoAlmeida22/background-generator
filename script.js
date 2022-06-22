db62f0a75c7fe27ef0963ecb81526e17091bbbee
DiogoAlmeida22 <diogomiguel22@gmail.com>
2022-06-22 19:35:34 +0100

    atualizado

--- script.js
+++ script.js
@@ -1,54 +1,15 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var li = document.getElementsByTagName("li");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
-function inputLength(){
-	return input.value.length;
+function setGradient()
+{
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value + ", " + color2.value + ")";
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+color1.addEventListener("input", setGradient);
 
-	var button = document.createElement("button");
-	button.appendChild(document.createTextNode("Done!"));
-	li.appendChild(button);
-	button.onclick=underlineParent;
+color2.addEventListener("input", setGradient);
 
-	var button = document.createElement("button");
-
-	button.appendChild(document.createTextNode("Delete"));
-
-	li.appendChild(button);
-
-	button.onclick = removeParent;
-
-}
-
-ul.onclick = function(event){
-	var target = event.target;
-	target.classList.toggle("done");
-}
-
-function removeParent(evt){
-	evt.target.parentNode.remove()
-}
-
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
